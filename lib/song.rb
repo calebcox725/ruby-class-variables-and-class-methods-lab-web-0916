fd6aac442a303816c554a8cdc311a9890de21d63
calebcox725 <caleb.cox@flatironschool.com>
2016-09-29 18:38:19 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each_with_object({}) do |genre, counts|
+      counts[genre] ||= 0
+      counts[genre] += 1
+    end
+  end
+
+  def self.artist_count
+    @@artists.each_with_object({}) do |artist, counts|
+      counts[artist] ||= 0
+      counts[artist] += 1
+    end
+  end
+end
